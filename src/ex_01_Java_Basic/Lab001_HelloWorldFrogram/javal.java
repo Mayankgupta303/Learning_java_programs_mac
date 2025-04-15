f70457658b903a65aea4a967a6d2e42a98852562
mayankgupta <gargmayank3030@gmail.com>
2025-04-15 17:52:09 +0530

    First Commit

--- src/ex_01_Java_Basic/Lab001_HelloWorldFrogram/javal.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldFrogram/javal.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic.Lab001_HelloWorldFrogram;
 public class javal {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
