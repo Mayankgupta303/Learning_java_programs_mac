b374cff9297d08a3f22e693485237940ee86ebdd
mayankgupta <gargmayank3030@gmail.com>
2025-04-19 18:59:01 +0530

    Learning diff between println and print 19Apr

--- src/ex_01_Java_Basic/HelloWorld.java
+++ src/ex_01_Java_Basic/HelloWorld.java
@@ -3,5 +3,8 @@ package ex_01_Java_Basic;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+//        System.out.println("Hello World");
+//        System.out.println("Hello World");
     }
 }
---
+++ src/ex_01_Java_Basic/lab01.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class lab01 {
+    public static void main(String[] args) {
+    //    System.out.println("Hello testing");
+
+        System.out.print("Hello test");
+        System.out.print("Hello test");
+    }
+}
