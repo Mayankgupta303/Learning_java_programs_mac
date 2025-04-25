fe4ac3ce5e9d04378b632bf2f83b99e2e971ff76
mayankgupta <gargmayank3030@gmail.com>
2025-04-25 16:42:14 +0530

    Learning diff between println and print 19Apr

---
+++ src/ex_01_Java_Basic/lab003Variable.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class lab003Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.printf("%d",a);
+
+        int b = 20;
+        System.out.println("---");
+        System.out.printf("%d + %d ",a,b);
+    }
+}
