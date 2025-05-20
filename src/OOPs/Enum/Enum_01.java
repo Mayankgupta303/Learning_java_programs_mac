0fe965bf4f21169b16c2ed67f61417f5ae590413
mayankgupta <gargmayank3030@gmail.com>
2025-05-20 05:50:58 +0530

    Learning Enum

---
+++ src/OOPs/Enum/Enum_01.java
@@ -0,0 +1,11 @@
+package OOPs.Enum;
+
+public class Enum_01 {
+    public static void main(String[] args) {
+        System.out.println(day.Friday);
+    }
+}
+
+enum day{
+    Mon , Tuesday , Wednesday , Thrusday , Friday , Saturday , Sunday
+        }
